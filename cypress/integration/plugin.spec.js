7b384b6d9d830177d5e6af8c35578890c3709402
NoriSte <nori.ste.magni@gmail.com>
2019-04-27 12:11:36 +0200

    test: 💍 Add the basic test suite

---
+++ cypress/integration/plugin.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+context('Actions', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5000/')
+  })
+
+  it('Should work with an immediately-satisfied condition', () => {
+
+  })
+
+  it('Should work with a condition satisfied after a random delay', () => {
+
+  })
+
+  it('Should make the test fail with an unsatisfied condition', () => {
+
+  })
+})
