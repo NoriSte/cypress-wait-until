30c53e2857d57d4110121dc0708bb8dc12de97e3
NoriSte <nori.ste.magni@gmail.com>
2019-08-16 16:54:59 +0200

    feat: 🎸 Allow error message customization
    Closes: 35

--- cypress/types/plugin.spec.ts
+++ cypress/types/plugin.spec.ts
@@ -10,8 +10,12 @@ cy.waitUntil(() => false, {})
 cy.waitUntil(() => Promise.resolve(true), {})
 cy.waitUntil(() => Promise.resolve(false), {})
 
-
 cy.waitUntil(() => true, { timeout: 500 })
-cy.waitUntil(() => false)
-cy.waitUntil(() => Promise.resolve(true))
-cy.waitUntil(() => Promise.resolve(false))
+cy.waitUntil(() => false, { timeout: 500 })
+cy.waitUntil(() => Promise.resolve(true), { timeout: 500 })
+cy.waitUntil(() => Promise.resolve(false), { timeout: 500 })
+
+cy.waitUntil(() => true, { errorMsg: "Custom error message" })
+cy.waitUntil(() => false, { errorMsg: "Custom error message" })
+cy.waitUntil(() => Promise.resolve(true), { errorMsg: "Custom error message" })
+cy.waitUntil(() => Promise.resolve(false), { errorMsg: "Custom error message" })
