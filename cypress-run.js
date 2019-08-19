41c8b4e711ff906b982d6acba4ef6fd975247b8b
NoriSte <nori.ste.magni@gmail.com>
2019-08-19 12:45:46 +0200

    ci: 🎡 Run the Cypress executable through npx

--- cypress-run.js
+++ cypress-run.js
@@ -1,6 +1,6 @@
 const {execSync} = require('child_process');
 const uploadRecordings = process.env.npm_package_config_cypressUploadRecordings;
-let command = "cypress run";
+let command = "npx cypress run";
 if(uploadRecordings) {
     command += ` --record --key ${process.env.CYPRESS_RECORD_KEY}`
 }
